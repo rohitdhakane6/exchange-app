f424094fbea201b6f9a27960249b5b1587c2f155
Rohit Dhakane <rohitdhakane6@gmail.com>
2024-07-02 17:41:55 +0530

    teilwind config updated

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,9 +3,43 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
+const config: Pick<Config, "content" | "presets"| "theme"> = {
   content: ["./src/app/**/*.tsx"],
   presets: [sharedConfig],
+  theme: {
+    fontSize: {
+      xs: "0.7rem",
+      sm: "0.8rem",
+    },
+    extend: {
+      colors: {
+        greenBackgroundTransparent: 'rgba(0,194,120,.12)',
+        redBackgroundTransparent: 'rgba(234,56,59,.12)',
+        baseBackgroundL2: "rgb(32,33,39)",
+        baseBackgroundL3: "rgb(32,33,39)",
+        greenPrimaryButtonBackground: "rgb(0,194,120)",
+        baseBackgroundL1: "rgb(20,21,27)",
+        // text-greenText:"rgb(0,194,120);"
+        
+      },
+      borderColor: {
+        redBorder: 'rgba(234,56,59,.5)',
+        greenBorder: 'rgba(0,194,120,.4)',
+        baseBorderMed: '#cccccc',
+        accentBlue: "rgb(76,148,255)",
+        baseBorderLight: "rgb(32,33,39)",
+        baseTextHighEmphasis: "rgb(244,244,246)"
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      textColor: {
+        greenPrimaryButtonText: "rgb(20,21,27)"
+      }
+    },
+  },
 };
 
 export default config;
